e178f718a96855b8f382a4be568cfb10ca3eb00b
HacDan <david.mcclellan.p@gmail.com>
2022-06-23 20:52:11 -0400

    Updated logging to log denominator before crash

--- app/src/main/java/org/hacdan/debugging/MainActivity.kt
+++ app/src/main/java/org/hacdan/debugging/MainActivity.kt
@@ -2,10 +2,31 @@ package org.hacdan.debugging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logging()
+        division()
+    }
+
+    fun logging() {
+        Log.v(TAG,"Hello, world!")
+    }
+
+    fun division() {
+        var numerator = 60
+        var denominator = 4
+
+        repeat(5) {
+            Log.d(TAG, "$denominator")
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
     }
 }
